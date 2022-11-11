549522482788b441e65d26339d700822f7ce6f72
TimeAnomalyCode <77450287+TimeAnomalyCode@users.noreply.github.com>
2022-11-11 12:25:32 +0800

    updated item class

--- Item.java
+++ Item.java
@@ -3,18 +3,7 @@ public class Item {
     private int m_weight;
 
     Item(String productId, int weight){
-
         m_productId = productId;
-
-        if(weight < 0){
-            m_weight = 0;
-        }
-        else {
-            m_weight = weight;
-        }
-    }
-
-    public boolean equals(Item itm){
-        return itm.m_productId.equals(this.m_productId);
+        m_weight = weight;
     }
 }
