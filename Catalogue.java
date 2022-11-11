202260b975d38026f25b74d20bd617678ab608c7
TimeAnomalyCode <77450287+TimeAnomalyCode@users.noreply.github.com>
2022-11-11 15:50:03 +0800

    added most methods in Catalogue.java added Test.java for testing methods

--- Catalogue.java
+++ Catalogue.java
@@ -1,27 +1,85 @@
+import java.util.Scanner;
 public class Catalogue {
-    private String[] m_products;
+    private Products[] m_products;
 
-    public void addProducts(){
-
+    public Catalogue() {
+        m_products = new Products[100];
     }
 
-    public void deleteProducts(){
-
+    public void addProducts(Products p){
+        for(int i = 0; i < 100; i++){
+            if(m_products[i] == null){
+                m_products[i] = p;
+                System.out.println("Product Added");
+                break;
+            }
+        }
     }
 
-    public void modifyProducts(){
+    public void deleteProducts(String id){
+        for(int i = 0; i < 100; i++){
+            if(m_products[i].getId().equals(id)){
+                m_products[i] = null;
+                break;
+            }
+        }
+    }
 
+    public void modifyProducts(String id){
+        Scanner sc = new Scanner(System.in);
+        String newId,newName,newCategory;
+        double newPrice;
+
+        for(int i = 0; i < 100; i++){
+            if(m_products[i].getId().equals(id)){
+                System.out.print("Enter ID: ");
+                newId = sc.nextLine();
+                System.out.print("Enter Name: ");
+                newName = sc.nextLine();
+                System.out.print("Enter Category: ");
+                newCategory = sc.nextLine();
+                System.out.print("Enter Price: ");
+                newPrice = sc.nextDouble();
+                sc.nextLine();
+                break;
+            }
+        }
     }
 
     public void loadProducts(){
 
     }
 
-    public void getProduct(){
-
+    public Products getProduct(String id){
+        int index = 0;
+        for(int i = 0; i < 100; i++){
+            if(m_products[i].getId().equals(id)){
+                index = i;
+                break;
+            }
+        }
+        return m_products[index];
     }
 
-    public void displayProduct(){
-
+    public void displayProduct(String id){
+        for(int i = 0; i < 100; i++){
+            if(m_products[i].getId().equals(id)){
+                System.out.println("Product ID:" + m_products[i].getId());
+                System.out.println("Name:" + m_products[i].getName());
+                System.out.println("Category:" + m_products[i].getCategory());
+                System.out.println("Price:" + m_products[i].getPrice());
+                break;
+            }
+        }
     }
 }
+
+//Notes
+/*
+Catalogue ctl = new Catalogue();
+Products p1 = new Products("WC001","White Chocolate","White Chocolate",5.0);
+Products p2 = new Products("DC001","Dark Chocolate","Dark Chocolate",5.6);
+
+ctl.addProducts(p1);
+System.out.println(ctl.getProduct("WC001").getName());
+*/
---
+++ Test.java
@@ -0,0 +1,8 @@
+//This is used to test if a class works
+//It is to be removed after the project is completed
+//Please remove any code after testing
+public class Test {
+    public static void main(String args[]){
+
+    }
+}
