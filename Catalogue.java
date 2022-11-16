d70767919455d144ed445f4843e6271c45dcb578
KianFoh <thienkianfoh@gmail.com>
2022-11-16 10:14:06 +0800

    Added:Save Products,Load Products and Display Products

--- Catalogue.java
+++ Catalogue.java
@@ -1,14 +1,20 @@
+
 import java.util.Scanner;
+import java.util.Arrays;
+import java.io.*;
+
 public class Catalogue {
+
     private Products[] m_products;
 
     public Catalogue() {
         m_products = new Products[100];
     }
 
-    public void addProducts(Products p){
-        for(int i = 0; i < 100; i++){
-            if(m_products[i] == null){
+    public void addProducts(Products p) {
+        
+        for (int i = 0; i < 100; i++) {
+            if (m_products[i] == null) {
                 m_products[i] = p;
                 System.out.println(m_products[i].getProductId() + " Added to Catalogue");
                 break;
@@ -16,22 +22,22 @@ public class Catalogue {
         }
     }
 
-    public void deleteProducts(String id){
-        for(int i = 0; i < 100; i++){
-            if(m_products[i].getProductId().equals(id)){
+    public void deleteProducts(String id) {
+        for (int i = 0; i < 100; i++) {
+            if (m_products[i].getProductId().equals(id)) {
                 m_products[i] = null;
                 break;
             }
         }
     }
 
-    public void modifyProducts(String id){
+    public void modifyProducts(String id) {
         Scanner sc = new Scanner(System.in);
-        String newId,newName;
+        String newId, newName;
         double newPrice;
 
-        for(int i = 0; i < 100; i++){
-            if(m_products[i].getProductId().equals(id)){
+        for (int i = 0; i < 100; i++) {
+            if (m_products[i].getProductId().equals(id)) {
                 System.out.print("Enter ID: ");
                 newId = sc.nextLine();
                 m_products[i].setProductId(newId);
@@ -47,24 +53,112 @@ public class Catalogue {
         }
     }
 
-    public void loadProducts(){
+    //Save all products in catalogue Products array into Products.txt
+    public void saveProducts() throws IOException {
+        System.out.println("Saving Product file...");
+        try {
+            PrintWriter outputFile = new PrintWriter("Products.txt");
+            for (int i = 0; i < 100; i++) {
+                if (m_products[i] == null) {
+                    break;
+                }
+                outputFile.println("ProductId:" + m_products[i].getProductId());
+                outputFile.println("ProductName:" + m_products[i].getName());
+                outputFile.println("ProductPrice:" + +m_products[i].getPrice());
+                outputFile.print("\n");
+            }
+            outputFile.close();
+            System.out.println("Products file Saved");
+        } catch (Exception e) {
+            System.out.print("Failed to save Products file");
+        }
+    }
+
+    //Run loadProducts data from Products.txt 
+    public void loadProducts() throws IOException {
+
+        //Line of strings contain Id,Name,Price
+        String productIdline;
+        String productNameline;
+        String productPriceline;
+
+        //Use arrays because split method return arrays
+        String[] productIdarr;
+        String[] productNamearr;
+        String[] productPricearr;
+
+        //Final data
+        String productId;
+        String productName;
+        double productPrice;
+        File file = new File("Products.txt");
+
+        try {
+            Scanner inputFile = new Scanner(file);
+
+            if (file.length() == 0) {
+                System.out.println("Products.txt is empty");
+            }
+
+            for (int i = 0; i < 100 && inputFile.hasNext(); i++) {
+
+                productIdline = inputFile.nextLine();
+                productNameline = inputFile.nextLine();
+                productPriceline = inputFile.nextLine();
+                inputFile.nextLine();
+
+                productIdarr = productIdline.split(":");
+                productNamearr = productNameline.split(":");
+                productPricearr = productPriceline.split(":");
+
+                productId = productIdarr[1];
+                productName = productNamearr[1];
+                productPrice = Double.parseDouble(productPricearr[1]);
+
+                Products product = new Products(productId, productName, productPrice);
+
+                m_products[i] = product;
+            }
+            inputFile.close();
+        } catch (Exception e) {
+            if (!file.exists()) {
+                System.out.println("Products.txt does not exist");
+            } else {
+                System.out.println("Failed to load Products file");
+            }
+        }
 
     }
 
-    public Products getProduct(String id){
+    public Products getProduct(String id) {
         int index = 0;
-        for(int i = 0; i < 100; i++){
-            if(m_products[i].getProductId().equals(id)){
+        for (int i = 0; i < 100; i++) {
+            if (m_products[i].getProductId().equals(id)) {
                 index = i;
                 break;
             }
         }
         return m_products[index];
     }
+    
+    //Display all Products in m_products array
+    public void ListProducts() {
+        for (int i = 0; i < 100; i++) {
+            if (m_products[i] != null) {
+                System.out.println("Product " + (i + 1));
+                System.out.println("Product ID:" + m_products[i].getProductId());
+                System.out.println("Name:" + m_products[i].getName());
+                System.out.println("Price:" + m_products[i].getPrice());
+                System.out.print("\n");
+                continue;
+            }
+            break;
+        }
+    }
 
-    public void displayProduct(String id){
-        for(int i = 0; i < 100; i++){
-            if(m_products[i].getProductId().equals(id)){
+    public void displayProduct(String id) {
+        for (int i = 0; i < 100; i++) {
+            if (m_products[i].getProductId().equals(id)) {
                 System.out.println("Product ID:" + m_products[i].getProductId());
                 System.out.println("Name:" + m_products[i].getName());
                 System.out.println("Price:" + m_products[i].getPrice());
@@ -77,9 +171,11 @@ public class Catalogue {
 //Notes
 /*
 Catalogue ctl = new Catalogue();
+ctl.loadProducts();
 Products p1 = new Products("WC001","White Chocolate","White Chocolate",5.0);
 Products p2 = new Products("DC001","Dark Chocolate","Dark Chocolate",5.6);
 
 ctl.addProducts(p1);
+ctl.saveProducts();
 System.out.println(ctl.getProduct("WC001").getName());
-*/
+ */
--- Test.java
+++ Test.java
@@ -1,17 +1,24 @@
+
+import java.io.IOException;
+
 //This is used to test if a class works
 //It is to be removed after the project is completed
 //Please remove any code after testing
 public class Test {
-    public static void main(String args[]){
+    public static void main(String args[]) throws IOException{
         Catalogue ctl = new Catalogue();
-        Order od = new Order(ctl);
+        ctl.loadProducts();
+//        Order od = new Order(ctl);
         Products p1 = new Products("WC","White Chocolate",5.0);
         Products p2 = new Products("DC","Dark Chocolate",5.6);
 
         ctl.addProducts(p1);
-        ctl.addProducts(p2);
-        od.createOrder();
+        ctl.addProducts(p2);       
+        ctl.ListProducts();
 
-        od.printOrder("S10001");
+//        od.createOrder();
+//        od.printOrder("S10001");
+        
+        ctl.saveProducts();
     }
 }
